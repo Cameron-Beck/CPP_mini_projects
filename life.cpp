5db22d163deb6399e07396e1e1f5e2699f834ec1
Cameron_Beck <cameron.beckit@gmai.com>
2023-02-13 21:11:46 +0000

    small changes

--- life.cpp
+++ life.cpp
@@ -9,7 +9,8 @@ main(){
     lowEffortAction = 1.0;
     cout << "XP = " << xp << endl;
     xp + lowEffortAction;
-    cout << "XP = " << xp << endl;
+    cout << "space" << endl;
+    cout << "XP = " << xp + lowEffortAction << endl;
 
 
     return 0;
