6e8cd976154813898b2c8847e7cd943dfcbf2d66
Cameron-Beck <camero.beckit@gmail.com>
2023-03-12 20:26:55 +0000

    added nfunct

---
+++ nfunct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    std::cout << sqrt(-25) << std::endl;
+    std::cout << pow(10000, 10000000) << std::endl;
+    std::cout << pow(-9, 999) <<std::endl;
+    std::cout << remainder(10, 3.25) << std::endl;
+    std::cout << fmax(10, 20) << std::endl;
+
+
+    return 0;
+}
