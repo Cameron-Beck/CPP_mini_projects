847113163ab227dcf42f1c9b8517272a486cd85a
Cameron-Beck <camero.beckit@gmail.com>
2023-03-12 17:43:54 +0000

    added float

---
+++ float.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+int main(){
+
+    float a = 10.0 / 3;
+    a = a * 10000000;
+    double b = 77E4;
+    
+    std::cout << std::fixed << a << std::endl;
+
+
+
+    return 0;
+}
