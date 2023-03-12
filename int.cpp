22de9776ac4fbab17956b0d47ed16927b5a78bc7
Cameron-Beck <camero.beckit@gmail.com>
2023-03-12 14:43:10 +0000

    added int

---
+++ int.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+
+
+int main (){
+
+    short a;
+    int b;
+    long c;
+    long long d;
+    unsigned short aa;
+    unsigned int bb;
+    unsigned long cc;
+    unsigned long long dd;
+
+    std::cout << SHRT_MAX << std::endl;
+    std::cout << SHRT_MIN << std::endl;
+    std::cout << USHRT_MAX << std::endl;
+
+    return 0;
+}
