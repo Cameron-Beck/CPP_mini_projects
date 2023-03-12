f971604f994a1e6a6cda3849920f879e8e1e348d
Cameron-Beck <camero.beckit@gmail.com>
2023-03-12 15:20:20 +0000

    added escape

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main (){
+
+std::cout << "hello \t there";
+std::cout << "hello \a there";
+std::cout << "hello \b there";
+std::cout << "hello \"there\" ' " ;
+std::cout << "hello there\0";
+    return 0;
+}
