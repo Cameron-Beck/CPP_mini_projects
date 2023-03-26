58adca88c8dfcdaec0d3513a5105af2894d0f0de
Cameron-Beck <camero.beckit@gmail.com>
2023-03-26 20:00:47 +0100

    adds guessing game to menu

--- menu.cpp
+++ menu.cpp
@@ -1,24 +1,53 @@
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void play_game(){
+
+    int random = rand() % 251;
+    std::cout << "you are playing\n";
+    std::cout << random << std::endl;
+    std::cout << "Guess a number" << std::endl;
+    while(true)
+    {
+        int guess;
+        std::cin >> guess;
+        if (guess == random)
+        {
+            std::cout << "You win" << std::endl;
+            break;
+        } else if (guess < random)
+        {
+            std::cout << "number to low" << std::endl;
+        } else
+        {
+            std::cout << "number to high" << std::endl;
+        }
+
+    }
+}
 
 int main()
-{
-    int choice;
-    do
-    {
-    std::cout << "0. Quit" << std::endl << "1. Play Game" << std::endl ;
-    std::cin >> choice;
-    }
-    while(choice != 0);
+{   
+    srand(time(NULL));
+    int choice;    
+    do{
+        std::cout << "0.Quit" << std::endl << "1.Play game" << std::endl;
+        std::cin >> choice;
 
         switch(choice)
         {
             case 0:
-                std::cout << "Exited";
-                return 0;
+            std::cout << "You have selected quit" << std::endl ;
+            return 0;
+
             case 1:
-                std::cout << "Selected play";
-                break;
+            play_game();
+            break;
         }
+    }
+    while(choice != 0);
+
 
 }
