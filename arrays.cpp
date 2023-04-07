482b5554c6cb8be1b752ff0198b06133952c7d53
Cameron-Beck <camero.beckit@gmail.com>
2023-04-07 18:01:28 +0100

    arrays

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main()
+{
+
+    int guesses[] = {10,4,23,52,43,12,11,18,42};
+    std::cout << guesses[3] << std::endl;
+    guesses[3] = 300;
+    std::cout << guesses[3] << std::endl;
+
+
+
+
+}
